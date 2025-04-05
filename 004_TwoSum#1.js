c6b2d6b233637708cb143226ae6bce4c76f1e6b7
Mauli Mogal <maulimogal@gmail.com>
2025-04-06 00:06:52 +0530

    #1

---
+++ 004_TwoSum#1.js
@@ -0,0 +1,42 @@
+// Brute-Force
+
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function(nums, target) {
+    for (let i = 0; i < nums.length; i++) {
+        for (let j = i + 1; j < nums.length; j++) {
+            if (nums[i] + nums[j] === target) {
+                return [i, j];
+            }
+        }
+    }
+    return [];
+
+};
+
+// Hash Map
+
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function(nums, target) {
+    const map = {}; // Stores number as key and its index as value
+
+    for (let i = 0; i < nums.length; i++) {
+        const complement = target - nums[i];
+
+        if (map.hasOwnProperty(complement)) {
+            return [map[complement], i];
+        }
+
+        map[nums[i]] = i;
+    }
+
+    return []; // If no solution is found
+};
+
